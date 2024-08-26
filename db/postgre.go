cfdaf0c5ae6b96f73a1240fae22840f542ac2bb4
Dav He <s991179@gmail.com>
2024-08-27 00:09:27 +0800

    feat: set connection pool

--- db/postgre.go
+++ db/postgre.go
@@ -1,6 +1,8 @@
 package db
 
 import (
+	"time"
+
 	"go.uber.org/zap"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -24,6 +26,18 @@ func (Postgre) GetGormDb(connectString string, fileLogger *zap.Logger) *gorm.DB 
 		fileLogger.Error(err.Error())
 		panic("Db connect failed!")
 	}
+
+	// Get the generic database object sql.DB to configure connection pool
+	sqlDB, err := db.DB()
+	if err != nil {
+		fileLogger.Error(err.Error())
+		panic("Failed to get sql.DB from gorm.DB")
+	}
+
+	// Configure the connection pool
+	sqlDB.SetMaxIdleConns(10)           // Set the maximum number of idle connections
+	sqlDB.SetMaxOpenConns(100)          // Set the maximum number of open connections
+	sqlDB.SetConnMaxLifetime(time.Hour) // Set the maximum connection lifetime
 
 	return db
 }
