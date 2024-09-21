6bd340dadcb520cb78f150fafad59909dfb5d6d2
Dav He <s991179@gmail.com>
2024-09-21 23:53:48 +0800

    feat: build image and push

---
+++ build.sh
@@ -0,0 +1,11 @@
+DockerHubUrl="localhost:5000"
+Tag=$1
+
+ImageName="$DockerHubUrl/gin-test1:${Tag}"
+
+# build cache
+docker builder prune -f
+docker image prune -f
+
+docker build -t "$ImageName" &&
+docker image push "$ImageName"
