7e42c035eeb6a1afd64cc1ed1185604890c0eefb
Dav He <s991179@gmail.com>
2024-10-02 02:29:10 +0800

    chore: set variable

--- run.sh
+++ run.sh
@@ -5,10 +5,9 @@ Tag=$1
 ImageName="$DockerHubUrl/gin-test1:${Tag}"
 
 (
-    export ImageName=$ImageName
-    export NodePort=30080
-    
-    helm upgrade --install gin-test1 helm-chart/
+    helm upgrade --install gin-test1 helm-chart/ \
+    --set service.nodePort=30080 \
+    --set service.imageName=$ImageName
 
     # docker-compose pull
     # docker-compose up -d
