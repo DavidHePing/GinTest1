78790954143606f90c310fbcc8fb36e8be87c54c
Dav He <s991179@gmail.com>
2024-10-02 02:05:27 +0800

    chore: deploy gin test image

--- run.sh
+++ run.sh
@@ -1,13 +1,15 @@
 DockerHubUrl="localhost:5000"
 Tag=$1
 
-helm upgrade --install helm-demo2 helm-chart/
 
-# ImageName="$DockerHubUrl/gin-test1:${Tag}"
+ImageName="$DockerHubUrl/gin-test1:${Tag}"
 
-# (
-#     export ImageName=$ImageName
+(
+    export ImageName=$ImageName
+    export NodePort=30080
+    
+    helm upgrade --install gin-test1 helm-chart/
 
-#     docker-compose pull
-#     docker-compose up -d
-# )
+    # docker-compose pull
+    # docker-compose up -d
+)
