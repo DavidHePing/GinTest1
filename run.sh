f60b327dfd3ee4d9b1e8e9a3a07a422b988984e5
Dav He <s991179@gmail.com>
2024-10-02 02:35:47 +0800

    fix: use real structure name

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ ImageName="$DockerHubUrl/gin-test1:${Tag}"
 (
     helm upgrade --install gin-test1 helm-chart/ \
     --set service.nodePort=30080 \
-    --set service.imageName=$ImageName
+    --set image.repository=$ImageName
 
     # docker-compose pull
     # docker-compose up -d
