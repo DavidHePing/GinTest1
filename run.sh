5341fa4c7ca4bb21a911291ca80a464a449d5041
Dav He <s991179@gmail.com>
2024-10-02 02:54:27 +0800

    chore: use /health for health check

--- run.sh
+++ run.sh
@@ -5,6 +5,8 @@ Tag=$1
 ImageName="$DockerHubUrl/gin-test1:${Tag}"
 
 (
+    docker pull $ImageName
+
     helm upgrade --install gin-test1 helm-chart/ \
     --set service.nodePort=30080 \
     --set image.repository=$ImageName
